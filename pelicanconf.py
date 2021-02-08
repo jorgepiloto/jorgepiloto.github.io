7a3b5874bf5dc0e68e5719e0a0bae70dabe7bb3f
Jorge M.G <jorge@on-orbit.dev>
2021-02-08 12:09:53 +0100

    Improve configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'Jorge M.G.'
-SITENAME = 'On-orbit'
-SITEURL = ''
+AUTHOR = "Jorge M.G."
+SITENAME = "On-orbit"
+SITEURL = ""
 
-PATH = 'content'
 
-TIMEZONE = 'Europe/Madrid'
-
-DEFAULT_LANG = 'en'
+TIMEZONE = "Europe/Madrid"
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -18,21 +16,57 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+# Menu links
+MENU_LINKS = (
+    ("About me", "fa-user-circle-o", "pages/about.html"),
+    ("Orbital Mechanics", "fa-rocket", "category/orbital-mechanics.html"),
+    ("Programming", "fa-laptop", "#"),
+    ("Projects", "fa-folder-o", "#"),
+    ("Books reviews", "fa-book", "#"),
+    ("Contact", "fa-envelope", "#"),
+)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Sidebar sections and links
+NEWS = (
+    (
+        "NumFocus accepted poliastro proposal on software validation",
+        """
+        The New validation framework for poliastro proposal was accepted by the
+        NumFocus organization under the small development grants program. During
+        the next four months, both Juan Luis and me will be working to tests the
+        accuracy of poliastro's routines against similar open-source projects.
+        """,
+    ),
+)
+INTERESTING_LINKS = (
+    ("The Astrobook", "https://astrobook.github.io/"),
+    ("Celestial mechancis journal", "https://www.springer.com/journal/10569"),
+    ("Degenerate Conic", "http://degenerateconic.com"),
+    ("De máquinas e intenciones", "https://demaquinaseintenciones.wordpress.com/"),
+    ("Numfys", "https://www.numfys.net/"),
+    ("Pybonacci", "https://pybonacci.org/"),
+)
+SIDEBAR_SECTIONS = (
+    ("News", "fa-newspaper-o", "fa-thumb-tack", NEWS),
+    ("Interesting websites", "fa-link", "fa-star", INTERESTING_LINKS),
+)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+# Keept articles structure after rendering website
+PATH = "content"
+STATIC_PATHS = ["resources"]
+PAGE_URL = "pages"
+ARTICLE_PATHS = [
+    "articles",
+]
+ARTICLE_URL = "articles/{date:%Y}/{date:%m}/{date:%d}/{slug}.html"
+ARTICLE_SAVE_AS = "articles/{date:%Y}/{date:%m}/{date:%d}/{slug}.html"
 
 # Custom theme for the website
-THEME = 'theme'
-THEME_STATIC_DIR = 'theme/static'
+THEME = "theme"
+CSS_FILE = "main.css"
+STATIC_PATHS.append("theme/static/css/")
